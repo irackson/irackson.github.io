268e4db696433794655245e2067588f467ab68ec
ianrackson <nasirus@protonmail.com>
2021-02-24 22:06:02 -0500

    ui show/hide functionality. project approved!

--- src/scripts/ts/index.ts
+++ src/scripts/ts/index.ts
@@ -53,3 +53,82 @@ customLog(
 // TODO: mobile design is minimal, but as screen gets bigger instructions are added
 
 /* display level of difficulty for each particular question: easy, medium, and hard. hover for definitions: medium and hard questions are worth twice as much as easy questions, and hard questions may given partial credit if there is more than one answer */
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const $playButton = $('#play-button');
+
+const $inactiveGameContainer = $('.inactive-game-container');
+const $activeGameContainer = $('.active-game-container'); // hidden onload
+const $player1Button = $('#player-1-button'); // hidden onload
+const $player2Button = $('#player-2-button'); // hidden onload
+
+const $modeContainer = $('.mode-container');
+const $devSelectButton = $('#dev-select-button');
+const $otdbSelectButton = $('#otdb-select-button');
+const $promptContainer = $('.prompt-container');
+
+const $devPregameContainer = $('.dev-pregame-container'); // hidden onload
+const $otdbPregameContainer = $('.otdb-pregame-container'); // hidden onload
+const $responseContainer = $('.response-container'); // hidden onload
+
+/* eslint-enable @typescript-eslint/no-unused-vars */
+let gameOn: boolean = false;
+let modeSelected: string | undefined;
+
+$devSelectButton.on('click', function (e) {
+	e.preventDefault();
+	$player1Button.hide();
+	$player2Button.hide();
+	$otdbPregameContainer.hide();
+	$devPregameContainer.show();
+
+	modeSelected = 'dev';
+});
+
+$otdbSelectButton.on('click', function (e) {
+	e.preventDefault();
+	$player1Button.hide();
+	$player2Button.hide();
+	$devPregameContainer.hide();
+	$otdbPregameContainer.show();
+
+	modeSelected = 'otdb';
+});
+
+$playButton.on('click', function (e) {
+	e.preventDefault();
+
+	if (gameOn) {
+		// reset to reload state
+
+		$player1Button.hide();
+		$player2Button.hide();
+		$activeGameContainer.hide();
+		$promptContainer.hide();
+		$responseContainer.hide();
+
+		$inactiveGameContainer.show();
+		$modeContainer.show();
+
+		gameOn = false;
+		modeSelected = undefined;
+	} else if (modeSelected !== undefined) {
+		// add condition for modeSelected === "otdb" & params not filled in by user
+		$devPregameContainer.hide();
+		$otdbPregameContainer.hide();
+		$inactiveGameContainer.hide();
+		$modeContainer.hide();
+
+		$player1Button.show();
+		$player2Button.show();
+		$activeGameContainer.show();
+		$promptContainer.show();
+		$responseContainer.show();
+
+		gameOn = true;
+		// call start game function...
+	} else {
+		console.log('hi');
+		// add bounce animation to choose game mode...
+	}
+});
