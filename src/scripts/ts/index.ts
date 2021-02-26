a2a14733e3ee829676d2ed47336e826da28fa37d
ianrackson <nasirus@protonmail.com>
2021-02-26 14:25:06 -0500

    network errors handled. time to startQuiz

--- src/scripts/ts/index.ts
+++ src/scripts/ts/index.ts
@@ -1,10 +1,7 @@
 /* eslint-disable */
 require('../../styles/styles.scss');
 import $ from 'jquery';
-import _ from 'lodash';
-import Chance from 'chance';
-
-const chance = new Chance();
+// import _ from 'lodash';
 
 /* eslint-enable */
 function customLog(message: any, color = 'black'): void {
@@ -44,7 +41,6 @@ customLog(
 //* use font-awesome
 //* consider bootstrap
 
-// TODO: question endpoint: https://cdn.aglty.io/3bikcueb/trivia/trivia-questions.json
 // TODO: each player has their own submit button
 // TODO: timed, with option to choose seconds/question vs. not timed
 // TODO: click to reveal hint if available
@@ -54,23 +50,9 @@ customLog(
 // TODO: fix fontawesome link bug
 // TODO: add bounce animation to CHOOSE and/or GAME MODE after play click w/out mode selected
 // TODO: add cool animation question mark to response container when game is inactive and no mode is selected thats visible on load
+// TODO: deal with error: otdp 'response_code: 1'... no trivia available. app should alert user and return them to otdp start page!!!
 
 /* display level of difficulty for each particular question: easy, medium, and hard. hover for definitions: medium and hard questions are worth twice as much as easy questions, and hard questions may given partial credit if there is more than one answer */
-
-interface OtdbParameters {
-	amount?: string;
-	category?: string;
-	difficulty?: string;
-	type?: string;
-}
-
-const playTrivia = function (otdbParameters?: OtdbParameters): void {
-	if (otdbParameters) {
-		console.log(otdbParameters);
-	} else {
-		console.log('dev');
-	}
-};
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 const $playButton = $('#play-button');
@@ -82,7 +64,9 @@ const $player2Button = $('#player-2-button'); // hidden onload
 
 const $modeContainer = $('.mode-container');
 const $devSelectButton = $('#dev-select-button');
+const $otdbDescriptionText = $('.otdb-description-text');
 const $otdbSelectButton = $('#otdb-select-button');
+const $promptText = $('#prompt');
 const $promptContainer = $('.prompt-container');
 
 const $devPregameContainer = $('.dev-pregame-container'); // hidden onload
@@ -98,8 +82,90 @@ const $responseContainer = $('.response-container'); // hidden onload
 let gameOn: boolean = false;
 let modeSelected: string | undefined;
 
+type OtdbParameters = {
+	amount: string;
+	category: string;
+	difficulty: string;
+	type: string;
+};
+
+const startQuiz = function (data: JSON): void {
+	console.log(data);
+	$promptText.text(modeSelected as string);
+};
+
+const playTrivia = function (otdbParameters?: OtdbParameters): void {
+	let url: string;
+	if (otdbParameters) {
+		const { amount, category, difficulty, type } = otdbParameters;
+
+		url = `https://opentdb.com/api.php?amount=${amount}${
+			category !== 'any' ? `&category=${category}` : ''
+		}${difficulty !== 'any' ? `&difficulty=${difficulty}` : ''}${
+			type !== 'any' ? `&type=${type}` : ''
+		}`;
+	} else {
+		url = 'https://cdn.aglty.io/3bikcueb/trivia/trivia-questions.json';
+	}
+
+	// let triviaData;
+
+	$.ajax({
+		url,
+	}).then(
+		(data) => {
+			if (otdbParameters && data.response_code !== 0) {
+				gameOn = false;
+
+				$player1Button.hide();
+				$player2Button.hide();
+				$activeGameContainer.hide();
+				$promptContainer.hide();
+				$responseContainer.hide();
+
+				$otdbDescriptionText.text(
+					'Sorry! No quiz in the Database matched these parameters. Try less questions and/or less specificity...'
+				);
+
+				$modeContainer.show();
+				$inactiveGameContainer.show();
+				$otdbPregameContainer.show();
+
+				$playButton.css('border', '1px solid goldenrod');
+			} else {
+				startQuiz(data);
+			}
+		},
+		(error) => {
+			console.log('bad request', error);
+
+			$player1Button.hide();
+			$player2Button.hide();
+			$activeGameContainer.hide();
+			$responseContainer.hide();
+
+			$inactiveGameContainer.show();
+			$modeContainer.show();
+
+			$devSelectButton.css('border', '1px solid black');
+			$otdbSelectButton.css('border', '1px solid black');
+			$playButton.css('border', '1px solid goldenrod');
+
+			$promptText.text(
+				'Sorry! Server error. Please check your internet connection or try a different game mode.'
+			);
+			$promptText.fadeIn();
+
+			gameOn = false;
+			modeSelected = undefined;
+		}
+	);
+};
+
 $devSelectButton.on('click', function (e) {
 	e.preventDefault();
+	$promptText.text('');
+	$promptContainer.hide();
 	$player1Button.hide();
 	$player2Button.hide();
 	$otdbPregameContainer.hide();
@@ -114,12 +180,17 @@ $devSelectButton.on('click', function (e) {
 $otdbSelectButton.on('click', function (e) {
 	e.preventDefault();
 
+	$promptText.text('');
+	$promptContainer.hide();
 	$player1Button.hide();
 	$player2Button.hide();
 	$devPregameContainer.hide();
 
 	$devSelectButton.css('border', '1px solid black');
 	$otdbSelectButton.css('border', '3px solid silver');
+	$otdbDescriptionText.html(
+		'Customize your match! Choose from over 4,000 verified questions housed in the <span class="nowrap">Open Trivia Database!</span>'
+	);
 
 	otdbParametersForm.reset();
 	$otdbPregameContainer.show();
@@ -129,8 +200,6 @@ $otdbSelectButton.on('click', function (e) {
 
 $playButton.on('click', function (e) {
 	e.preventDefault();
-	$devSelectButton.css('border', '1px solid black');
-	$otdbSelectButton.css('border', '1px solid black');
 
 	if (gameOn) {
 		// reset to reload state
@@ -144,6 +213,8 @@ $playButton.on('click', function (e) {
 		$inactiveGameContainer.show();
 		$modeContainer.show();
 
+		$devSelectButton.css('border', '1px solid black');
+		$otdbSelectButton.css('border', '1px solid black');
 		$playButton.css('border', '1px solid goldenrod');
 
 		gameOn = false;
@@ -169,32 +240,27 @@ $playButton.on('click', function (e) {
 			playTrivia();
 		}
 		if (modeSelected === 'otdb') {
-			let amount;
-			let category;
-			let difficulty;
-			let type;
+			const otdbParameters: OtdbParameters = {
+				amount: '',
+				type: '',
+				difficulty: '',
+				category: '',
+			};
 			const params = $('#otdb-parameters-form').serializeArray();
 			for (let i = 0; i < params.length; i++) {
 				if (params[i].name === 'otdb-num-input') {
-					amount = params[i].value;
+					otdbParameters.amount = params[i].value;
 				}
 				if (params[i].name === 'otdb-format-input') {
-					type = params[i].value;
+					otdbParameters.type = params[i].value;
 				}
 				if (params[i].name === 'otdb-diff-input') {
-					difficulty = params[i].value;
+					otdbParameters.difficulty = params[i].value;
 				}
 				if (params[i].name === 'otdb-cat-input') {
-					category = params[i].value;
+					otdbParameters.category = params[i].value;
 				}
 			}
-
-			const otdbParameters: OtdbParameters = {
-				amount,
-				category,
-				difficulty,
-				type,
-			};
 
 			playTrivia(otdbParameters);
 		}
