4cca2687198639e9056fd5c2d93cd3854afcdde9
ianrackson <nasirus@protonmail.com>
2021-03-04 13:37:31 -0500

    mobile browswer hide attempt 1

--- src/scripts/ts/index.ts
+++ src/scripts/ts/index.ts
@@ -6,7 +6,6 @@ import Player from './Player';
 import $ from 'jquery';
 import _ from 'lodash';
 
-/* eslint-enable */
 function customLog(message: any, color = 'black'): void {
 	switch (color) {
 		case 'success':
@@ -36,6 +35,48 @@ customLog(
 		${randomIntFromInterval(0, 255)},
 		${randomIntFromInterval(0, 255)})`
 );
+
+/*
+ * Normalized hide address bar for iOS & Android
+ * (c) Scott Jehl, scottjehl.com
+ * MIT License
+ */
+(function (win: any) {
+	var doc = win.document as any;
+	// If there's a hash, or addEventListener is undefined, stop here
+	if (!location.hash && win.addEventListener) {
+		//scroll to 1
+		window.scrollTo(0, 1);
+		var scrollTop = 1,
+			getScrollTop = function () {
+				return (
+					win.pageYOffset ||
+					(doc.compatMode === 'CSS1Compat' && doc.documentElement.scrollTop) ||
+					doc.body.scrollTop ||
+					0
+				);
+			},
+			//reset to 0 on bodyready, if needed
+			bodycheck = setInterval(function () {
+				if (doc.body) {
+					clearInterval(bodycheck);
+					scrollTop = getScrollTop();
+					win.scrollTo(0, scrollTop === 1 ? 0 : 1);
+				}
+			}, 15);
+		win.addEventListener('load', function () {
+			setTimeout(function () {
+				//at load, if user hasn't scrolled more than 20 or so...
+				if (getScrollTop() < 20) {
+					//reset to hide addr bar at onload
+					win.scrollTo(0, scrollTop === 1 ? 0 : 1);
+				}
+			}, 0);
+		});
+	}
+})(this);
+
+/* eslint-enable */
 
 //! ___          ___    ___  __      __   __   __   ___
 //!  |  |  |\/| |__      |  /  \    /  ` /  \ |  \ |__
